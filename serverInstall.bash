15ec49c90b612905c53f4cce59cf0174038b0977
Zelytra <alexbreuillet@gmail.com>
2022-09-29 16:57:52 +0200

    update docker composer

--- serverInstall.bash
+++ serverInstall.bash
@@ -2,6 +2,7 @@
 eval sudo rm -r ./z-demineur
 eval git clone https://github.com/zelytra/z-demineur.git
 
-eval docker build . -t z-demineur-client
-eval docker build ./server -t z-demineur-server
+eval docker build ./z-demineur/ -t z-demineur-client
+eval docker build ./z-demineur/server -t z-demineur-server
 
+eval docker-compose -f ./z-demineur/docker-compose.yml up
